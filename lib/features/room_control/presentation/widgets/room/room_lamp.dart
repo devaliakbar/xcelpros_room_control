13ebf071d55cc297f8f8d9a9fc4f63ac27f80bc7
Ali <dev.aliakbar@gmail.com>
2021-03-05 18:57:12 +0530

    Added Lamp

--- lib/features/room_control/presentation/widgets/room/room_lamp.dart
+++ lib/features/room_control/presentation/widgets/room/room_lamp.dart
@@ -9,39 +9,55 @@ class RoomLamp extends StatelessWidget {
       margin: EdgeInsets.only(
         right: SizeConfig.width(5),
       ),
-      child: Column(
+      child: Stack(
         children: [
-          AnimatedContainer(
-            duration: Duration(milliseconds: 500),
-            width: SizeConfig.width(1.5),
-            height: SizeConfig.heightWithoutSafeArea(9),
-            color: Colors.white,
-          ),
-          Image.asset(
-            AppImages.lamp,
-            width: SizeConfig.width(38),
-            fit: BoxFit.fitWidth,
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                Container(
+                  height: SizeConfig.width(3),
+                  width: SizeConfig.width(6),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColors.yellowBulb,
+                        blurRadius: SizeConfig.width(3),
+                        spreadRadius: SizeConfig.width(0.5),
+                      ),
+                    ],
+                    color: AppColors.yellowBulb,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(
+                        SizeConfig.width(3),
+                      ),
+                      bottomRight: Radius.circular(
+                        SizeConfig.width(3),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
           Container(
-            height: SizeConfig.width(3),
-            width: SizeConfig.width(6),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: AppColors.yellowBulb,
-                  blurRadius: SizeConfig.width(3),
-                  spreadRadius: SizeConfig.width(0.5),
+            margin: EdgeInsets.only(bottom: SizeConfig.width(3)),
+            child: Column(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  width: SizeConfig.width(1.5),
+                  height: SizeConfig.heightWithoutSafeArea(9),
+                  color: Colors.white,
+                ),
+                Image.asset(
+                  AppImages.lamp,
+                  width: SizeConfig.width(38),
+                  fit: BoxFit.fitWidth,
                 ),
               ],
-              color: AppColors.yellowBulb,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(
-                  SizeConfig.width(3),
-                ),
-                bottomRight: Radius.circular(
-                  SizeConfig.width(3),
-                ),
-              ),
             ),
           )
         ],
